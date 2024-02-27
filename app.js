20c23d5162af8ab789268513e638c0421904dc7e
Sameh <sameh.abdelhaleem95@gmail.com>
2024-02-27 18:56:32 +0200

    fix cors import

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 
 import bodyParser from "body-parser";
 import express from "express";
-const cors = require("cors");
+import cors from "cors";
 
 const app = express();
 app.use(cors());
